7b4c0657ee80b5aa9d863a29dfcd8b9bfa209346
David <david@glaesle.cloud>
2025-03-18 15:16:08 +0100

    use flake

--- flake.nix
+++ flake.nix
@@ -10,32 +10,22 @@
   };
 
   outputs = {
+    self,
     nixpkgs,
     espDev,
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
-    devShells.default = pkgs.mkShell {
-      buildInputs = [
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs =[
         espDev.packages.${system}.esp-idf-full
-        pkgs.python3
+        pkgs.python313
         pkgs.python3Packages.virtualenv
         pkgs.ffmpeg
+        pkgs.python313Packages.aubio
+        pkgs.python313Packages.pydub
       ];
-      shellHook = ''
-        # Create and activate a virtual environment if not already present.
-        if [ ! -d .venv ]; then
-          echo "Creating virtual environment..."
-          python3 -m venv .venv
-        fi
-        echo "Activating virtual environment..."
-        source .venv/bin/activate
-
-        # Upgrade pip and install required Python packages
-        pip install --upgrade pip
-        pip install aubio pydub
-      '';
     };
   };
 }
